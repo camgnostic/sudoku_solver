785ac20c4b3f89738797b1faeb4364dda137ca7f
Cameron McFee <cameron.mcfee@gmail.com>
2017-05-17 23:04:52 -0500

    subboard refactor into main class

--- main_class.py
+++ main_class.py
@@ -4,6 +4,7 @@ import math
 
 from helper_functions import (print_bare_board,
                               Contradiction,
+                              SubBoard,
                               Row,
                               Column,
                               Square,
@@ -13,23 +14,13 @@ class Board(object):
     """main class for holding a puzzle board and generating tools for solving"""
 
     def __init__(self, board):
-        self.rows = []
-        self.cols = []
-        self.squares = []
-        self.bare_board = board
-        size = int(math.sqrt(len(board)))
-        for row in board:
-            self.rows.append(Row(row))
-        for col in range(len(board[0])):
-            self.cols.append(Column([r[col] for r in board]))
-        for sqr, sqc in itertools.product(range(size), repeat=2):
-            self.squares.append(Square(get_square(board, sqr, sqc)))
+        self.board = SubBoard(board)
 
     def __str__(self):
-        return print_bare_board(self.bare_board)
+        return print_bare_board(self.board.bare_board)
 
     @property
     def solved(self):
-        return (all([row.solved for row in self.rows]) and
-                all([col.solved for col in self.cols]) and
-                all([square.solved for square in self.squares]))
+        return (all([row.solved for row in self.board.rows]) and
+                all([col.solved for col in self.board.cols]) and
+                all([square.solved for square in self.board.squares]))
