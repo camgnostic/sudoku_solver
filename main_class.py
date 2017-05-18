b969ef722160afcc213a6744f0549979b6990043
Cameron McFee <cameron.mcfee@gmail.com>
2017-05-17 21:24:14 -0500

    board class started

--- main_class.py
+++ main_class.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python
+
+class Board(object):
+    """main class for holding a puzzle board and generating tools for solving"""
+
+    def __init__(self, board):
+        pass
--- test_main_class.py
+++ test_main_class.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python
 
 import main_class as mc
+import unittest
+import sample_puzzles as puzzles
 
+class TestBoardClass(unittest.TestCase):
+    def test_class_exists(self):
+        board = mc.Board
+        
+    def test_board_loads_puzzle(self):
+        board = mc.Board(puzzles.simple_puzzle)
+
+
+if __name__ == '__main__':
+    unittest.main()
