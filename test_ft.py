d63b001a194f6cffdbc78f8107d2562d19ea6771
Cameron McFee <cameron.mcfee@gmail.com>
2017-05-21 17:13:51 -0500

    ft single square missing

--- test_ft.py
+++ test_ft.py
@@ -5,6 +5,7 @@ import sample_puzzles
 
 import unittest
 
+
 class TestSolvedPuzzles(unittest.TestCase):
     def test_solve_large(self):
         self.assertEqual(sample_puzzles.solved9,
@@ -13,6 +14,22 @@ class TestSolvedPuzzles(unittest.TestCase):
     def test_solve_small(self):
         self.assertEqual(sample_puzzles.solved4,
                          solver.solve(sample_puzzles.solved4))
+
+
+class TestOneSquarePuzzles(unittest.TestCase):
+    def get_puzzles(self, size):
+        for missing in ['corner', 'edge', 'middle']:
+            yield (sample_puzzles.one_square[size][missing],
+                   sample_puzzles.one_square[size]['solution'])
+
+    def test_solve_large(self):
+        for puzz, soln in self.get_puzzles('large'):
+            self.assertEqual(solver.solve(puzz), soln)
+
+    def test_solve_small(self):
+        for puzz, soln in self.get_puzzles('small'):
+            self.assertEqual(solver.solve(puzz), soln)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ test_ut.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import unittest
+
+
+class TestGetPossibilities(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
