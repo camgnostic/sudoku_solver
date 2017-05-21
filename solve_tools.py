35bf563924bf5c53b82aa5c3d206f47c4bffe388
Cameron McFee <cameron.mcfee@gmail.com>
2017-05-21 17:31:25 -0500

    tests for single spot

---
+++ solve_tools.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+def switcher(puzzle):
+    pass
+
+def find_single_spot(row, col):
+    pass
--- test_ut.py
+++ test_ut.py
@@ -1,11 +1,29 @@
 #!/usr/bin/python
 
 import unittest
+import solve_tools
+import sample_puzzles
 
 
-class TestGetPossibilities(unittest.TestCase):
-    pass
+class TestGetNextStep(unittest.TestCase):
+    """ chooses the next action based on the puzzle:
 
+        cases:
+            1. puzzle is solved (return None)
+            2. puzzle has 1 unsolved spot (return find_single_spot)
+    """
+    def test_solved_puzzle(self):
+        switcher = solve_tools.switcher
+        self.assertEqual(switcher(sample_puzzles.solved4), None)
+        self.assertEqual(switcher(sample_puzzles.solved9), None)
+
+    def test_one_unsolved_spot(self):
+        switcher = solve_tools.switcher
+        one_spot = solve_tools.find_single_spot
+        self.assertEqual(switcher(sample_puzzles.one_square['large']['corner']),
+                         one_spot(1,8))
+        self.assertEqual(switcher(sample_puzzles.one_square['small']['edge']),
+                         one_spot(3, 1))
 
 if __name__ == '__main__':
     unittest.main()
