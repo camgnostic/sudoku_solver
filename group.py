6593bbe6d4be6a537e63bf62f62c0699ec35bc12
Cameron McFee <cameron.mcfee@gmail.com>
2017-07-13 21:47:18 -0700

    Column getter function

--- group.py
+++ group.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+
 def solve_one(group):
+    """takes a list of 9 numbers with 1 zero, returns a list of 9 unique numbers with no zero"""
     if group.count(0) == 1:
         group[group.index(0)] = (set(range(1, 10)) - set(group)).pop()
     return group
+
+
+def get_column(colno, puzzle):
+    """takes a column number and a puzzle (list of lists) returns a list of values in that column"""
+    return [row[colno] for row in puzzle]
