3b9d99062edd3540fbdeefa473d8212cc31f2174
Cameron McFee <cameron.mcfee@gmail.com>
2017-07-13 21:37:19 -0700

    Use group function to pass ft

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
+import group
+
 def solve(puzzle):
+    for row_number in range(len(puzzle)):
+        puzzle[row_number] = group.solve_one(puzzle[row_number])
     return puzzle
