00c45a319dacd853eadf3bfaf438eab57d0b7ae3
Cameron McFee <cameron.mcfee@gmail.com>
2017-07-13 21:37:10 -0700

    Pass test

--- group.py
+++ group.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def solve_one(group):
-    group[group.index(0)] = (set(range(1, 10)) - set(group)).pop()
+    if group.count(0) == 1:
+        group[group.index(0)] = (set(range(1, 10)) - set(group)).pop()
     return group
