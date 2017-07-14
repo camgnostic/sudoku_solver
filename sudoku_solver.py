223ab118303119528d619f1cc65a7f9f9310f22a
Cameron McFee <cameron.mcfee@gmail.com>
2017-07-13 21:51:52 -0700

    Solves using column information

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -5,4 +5,7 @@ import group
 def solve(puzzle):
     for row_number in range(len(puzzle)):
         puzzle[row_number] = group.solve_one(puzzle[row_number])
+    for col_number in range(len(puzzle)):
+        group.set_column(col_number, puzzle,
+                         group.solve_one(group.get_column(col_number, puzzle)))
     return puzzle
