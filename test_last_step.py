cf9213e4bf941591a0e80891ed74276ed969271b
Cameron McFee <cameron.mcfee@gmail.com>
2017-07-13 21:39:12 -0700

    two steps tested

--- test_last_step.py
+++ test_last_step.py
@@ -27,6 +27,18 @@ one_step_from_solved = [
     [2, 3, 9, 8, 4, 1, 5, 6, 7]
 ]
 
+two_steps_from_solved = [
+    [0, 2, 7, 1, 5, 4, 3, 9, 6],
+    [9, 6, 5, 3, 2, 7, 1, 4, 8],
+    [3, 4, 1, 6, 8, 9, 7, 5, 2],
+    [5, 9, 3, 4, 6, 8, 2, 7, 1],
+    [4, 7, 2, 5, 1, 3, 6, 8, 9],
+    [6, 1, 8, 9, 0, 2, 4, 3, 5],
+    [7, 8, 6, 2, 3, 5, 9, 1, 4],
+    [1, 5, 4, 7, 9, 6, 8, 2, 3],
+    [2, 3, 9, 8, 4, 1, 5, 6, 7]
+]
+
 
 class TestSolvedPuzzle(unittest.TestCase):
     def test_solved_puzzle_is_returned(self):
@@ -36,3 +48,8 @@ class TestSolvedPuzzle(unittest.TestCase):
 class TestOneStepPuzzle(unittest.TestCase):
     def test_last_step_completed(self):
         assert sudoku_solver.solve(one_step_from_solved) == solution
+
+
+class TestTwoStepPuzzle(unittest.TestCase):
+    def test_last_two_steps_completed(self):
+        assert sudoku_solver.solve(two_steps_from_solved) == solution
